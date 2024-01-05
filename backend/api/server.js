5ef2403cfd927a70a14447cfb15b55ea8a5dfb01
Saga690 <master.ayush.2003@gmail.com>
2024-01-05 18:37:22 +0530

    User model

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,9 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 8800;
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB!');
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-app.listen(PORT, () => {
-    console.log(`Backend Server is listening at PORT ${PORT}`);
+app.listen(process.env.PORT, () => {
+    connectToDB();
+    console.log(`Backend Server is listening at PORT ${process.env.PORT}`);
 })

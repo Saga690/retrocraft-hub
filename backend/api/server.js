d23151a594e94f534eb863d0b28aba8c81afb615
Saga690 <master.ayush.2003@gmail.com>
2024-01-05 21:48:53 +0530

    Express API

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import gigRoute from "./routes/gig.route.js";
+import messageRoute from "./routes/message.route.js";
+import offerRoute from "./routes/offer.route.js";
+import reviewRoute from "./routes/review.route.js";
 
 dotenv.config();
 
@@ -14,6 +20,14 @@ const connectToDB = async () => {
         console.log(error);
     }
 }
+
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/offers", offerRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
+
 
 app.listen(process.env.PORT, () => {
     connectToDB();

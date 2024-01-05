2e32611ad5a52e0c41375c1af811b0ba4d65c0a2
Saga690 <master.ayush.2003@gmail.com>
2024-01-06 01:42:37 +0530

    password hashing

--- backend/api/controllers/auth.controller.js
+++ backend/api/controllers/auth.controller.js
@@ -1,11 +1,18 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 
 
 export const register = async (req,res) => {
 
     try {
-        const newUser = new User(req.body);
+
+        const salt = bcrypt.genSaltSync();
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        const newUser = new User({
+            ...req.body,
+            password: hash,
+        });
 
         await newUser.save();
         res.status(201).send("User has been created");

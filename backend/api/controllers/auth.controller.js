b81c3c7839f9c5b73b3195ad3a8ae7dae6d7f8c0
Saga690 <master.ayush.2003@gmail.com>
2024-01-08 02:09:09 +0530

    logout

--- backend/api/controllers/auth.controller.js
+++ backend/api/controllers/auth.controller.js
@@ -65,5 +65,8 @@ export const login = async (req, res, next) => {
 
 
 export const logout = async (req, res) => {
-
+    res.clearCookie("accessToken", {
+        sameSite: none,
+        secure: true,
+    }).status(200).send("User has been logged out");
 }

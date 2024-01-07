a59e3d95b9bbed5137983265e8e54a0bd1b27b71
Saga690 <master.ayush.2003@gmail.com>
2024-01-07 22:27:31 +0530

    changes

--- backend/api/controllers/user.controller.js
+++ backend/api/controllers/user.controller.js
@@ -1,3 +1,24 @@
-export const fn = (req,res) => {
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+
+
+export const deleteUser = async (req, res) => {
+
+    const user = await User.findById(req.params.id);
+
+    const token = req.cookies.accessToken;
+    if (!token) {
+        return res.status(401).send("You are not authenticated! Please authenticate and then try again.")
+    }
+
+    jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+        if (payload.id !== user._id.toString()) {
+            return res.status(403).send("You can delete only your account");
+        }
+    });
+
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("Account deleted successfully");
 
 }

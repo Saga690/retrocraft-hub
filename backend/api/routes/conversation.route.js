44754cefd4e09452c1f1dd9c52f98825e06e5f3c
Saga690 <master.ayush.2003@gmail.com>
2025-01-06 02:20:48 +0530

    created conversation endpoints

--- backend/api/routes/conversation.route.js
+++ backend/api/routes/conversation.route.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { createConversation, getConversations, getSingleConversations, updateConversation } from "../controllers/conversation.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/test", );
+router.get("/", verifyToken, getConversations);
+router.post("/", verifyToken, createConversation);
+router.get("/single/:id", verifyToken, getSingleConversations);
+router.put("/:id", verifyToken, updateConversation);
 
 export default router;

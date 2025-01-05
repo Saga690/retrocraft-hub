34a89d243dd6c43d3d2f569256913531569184cf
Saga690 <master.ayush.2003@gmail.com>
2025-01-05 21:01:06 +0530

    add review from frontend

--- client/components/Reviews/Reviews.js
+++ client/components/Reviews/Reviews.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Reviews.module.css'
 import Review from '../Review/Review.js'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import newRequest from '@/utils/newRequest';
 
 const Reviews = ({ gigId }) => {
+
+    const queryClient = useQueryClient();
 
     const { isLoading, error, data } = useQuery({
         queryKey: ['reviews'],
@@ -14,7 +16,22 @@ const Reviews = ({ gigId }) => {
             }),
     })
 
-    console.log("data is", data);
+    const mutation = useMutation({
+        mutationFn: (review) => {
+            return newRequest.post("/reviews", review);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries(["reviews"])
+        }
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const desc = e.target[0].value;
+        const star = e.target[1].value;
+        // console.log({ gigId, desc, star });
+        mutation.mutate({ gigId, desc, star });
+    }
 
     return (
         <div className={styles.reviews}>
@@ -22,6 +39,20 @@ const Reviews = ({ gigId }) => {
             {isLoading ? "loading..." : error ? "Something went wrong!" : data.map(review => (
                 <Review key={review._id} review={review} />
             ))}
+            <div className={styles.add}>
+                <h3>Add a review</h3>
+                <form action="" className={styles.inputForm} onSubmit={handleSubmit}>
+                    <input type="text"  className={styles.input} placeholder='what do you think?' />
+                    <select name="" id="" className={styles.select}>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                    </select>
+                    <button className={styles.formButton}>Send</button>
+                </form>
+            </div>
         </div>
     )
 }
